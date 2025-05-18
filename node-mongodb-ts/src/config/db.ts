33e0f6b91b0392739efccdc2095f741820e3b82f
Satendra Kumar Parteti <satendrakumarparteti.work@gmail.com>
2025-05-18 23:52:48 +0530

    added env

--- node-mongodb-ts/src/config/db.ts
+++ node-mongodb-ts/src/config/db.ts
@@ -4,7 +4,7 @@ dotenv.config();
 
 const connectDB = async (): Promise<void> => {
   try {
-    await mongoose.connect(process.env.MONGO_URI as string, {
+    await mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/node-mongodb-js" as string, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     } as any);
